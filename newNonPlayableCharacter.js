14194167f8d293a450648900ac4fe65af6b651c9
Jose <gldmz0318@icloud.com>
2022-11-28 20:23:24 -0800

    async walk east

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,9 +23,11 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    async function walkEast() {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        await sleep(2050)
+        stop()
     }
 
     function walkNorth() {
@@ -56,4 +58,10 @@ function newNonPlayableCharacter(x, y) {
         walkSouth: walkSouth,
         stop: stop
     }
+}
+
+function sleep(time){
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })  
 }
